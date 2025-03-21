162fe4346acbe690e0b36707ade42f70bc81c0e1
yudhalexa <101029025+yudhalexa@users.noreply.github.com>
2025-03-21 11:06:45 +0700

    test commit & branch, preview profile screen

--- app/src/main/java/id/ac/umn/ujournal/ui/profile/ProfileScreen.kt
+++ app/src/main/java/id/ac/umn/ujournal/ui/profile/ProfileScreen.kt
@@ -17,7 +17,9 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
+@Preview
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProfileScreen(
