6d5fcc5b3be3941e651918a061f4628a5576d796
yudhalexa <101029025+yudhalexa@users.noreply.github.com>
2025-03-24 10:31:11 +0700

    added + centered profile card

--- app/src/main/java/id/ac/umn/ujournal/ui/profile/ProfileScreen.kt
+++ app/src/main/java/id/ac/umn/ujournal/ui/profile/ProfileScreen.kt
@@ -1,12 +1,18 @@
 package id.ac.umn.ujournal.ui.profile
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -16,8 +22,20 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import coil3.compose.AsyncImage
+import id.ac.umn.ujournal.R
+
 
 @Preview
 @OptIn(ExperimentalMaterial3Api::class)
@@ -51,8 +69,58 @@ fun ProfileScreen(
         },
     ) { innerPadding: PaddingValues ->
         Column(
-            modifier = Modifier.padding(top = innerPadding.calculateTopPadding()).fillMaxSize(),
+            modifier = Modifier
+                .padding(top = innerPadding.calculateTopPadding())
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(15.dp, Alignment.CenterVertically)
         ) {
+            Card(
+                colors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                ),
+                modifier = Modifier
+                    .size(width = 300.dp, height = 450.dp)
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .padding(20.dp)
+                ) {
+                    Box {
+//                        Image(
+//                    painter = painterResource(id = R.drawable.)
+//                        contentDescription = " Profile Picture",
+//                        modifier = Modifier
+//                            .size(100.dp)
+//                            .clip(CircleShape)
+//                    )
+                        AsyncImage(
+                            model = "https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg",
+                            contentDescription = "Profile Picture",
+                            modifier = Modifier
+                                .size(100.dp)
+                                .clip(CircleShape)
+                        )
+                        Icon(
+                            imageVector = Icons.Filled.Edit,
+                            contentDescription = "Edit",
+                        )
+                    }
+                    Text(
+                        text = "John Doe",
+                        textAlign = TextAlign.Center,
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                    HorizontalDivider(color = MaterialTheme.colorScheme.secondary)
+                    Text(
+                        text = "Kelompok 3",
+                        textAlign = TextAlign.Center,
+                    )
+                }
+            }
+//            Spacer(Modifier.size(10.dp))
           Button(onClick = onLogoutButtonClick) {
               Text(text = "Logout")
           }
