701e72604a0da2d7f50be92ef27e6531224e76e9
Andi Usman Balo <andibalo213@gmail.com>
2025-04-01 05:09:50 +0700

    minor enhance atlas window info ui

--- app/src/main/java/id/ac/umn/ujournal/ui/map/MapScreen.kt
+++ app/src/main/java/id/ac/umn/ujournal/ui/map/MapScreen.kt
@@ -27,7 +27,6 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -151,7 +150,7 @@ fun MapScreen(
                             Text(
                                 text = selectedJournalEntry?.title!!,
                                 style = MaterialTheme.typography.titleMedium,
-                                color = MaterialTheme.colorScheme.onSurface
+                                color = MaterialTheme.colorScheme.primary
                             )
                         }
 
