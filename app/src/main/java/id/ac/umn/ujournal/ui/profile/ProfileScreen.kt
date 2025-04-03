ad6268c2405b2181e3c5662e9bd65211e80ee551
yudhalexa <101029025+yudhalexa@users.noreply.github.com>
2025-04-03 16:31:14 +0700

    new profile screen layout

--- app/src/main/java/id/ac/umn/ujournal/ui/profile/ProfileScreen.kt
+++ app/src/main/java/id/ac/umn/ujournal/ui/profile/ProfileScreen.kt
@@ -1,6 +1,7 @@
 package id.ac.umn.ujournal.ui.profile
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
@@ -22,6 +23,10 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Badge
+import androidx.compose.material.icons.filled.DarkMode
+import androidx.compose.material.icons.filled.Mail
 import androidx.compose.material3.*
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -43,7 +48,8 @@ fun ProfileScreen(
     navigateToLoginScreen : () -> Unit = {}
 ) {
     val userState by userViewModel.userState.collectAsState()
-    val user = (userState as UserState.Success).user
+//    val user = (userState as UserState.Success).user
+    val user = (userState as? UserState.Success)?.user // temp for preview
 
     fun onLogoutClick() {
         userViewModel.logout()
@@ -66,71 +72,140 @@ fun ProfileScreen(
         Column(
             modifier = Modifier
                 .padding(top = innerPadding.calculateTopPadding())
-                .fillMaxSize(),
+                .fillMaxSize()
+                .fillMaxHeight(),
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(15.dp, Alignment.CenterVertically)
+//            verticalArrangement = Arrangement.spacedBy(15.dp, Alignment.CenterVertically)
         ) {
-            Card(
-                colors = CardDefaults.cardColors(
-                    containerColor = MaterialTheme.colorScheme.surfaceVariant,
-                ),
+            Box(
                 modifier = Modifier
-                    .size(width = 300.dp, height = 450.dp)
+                    .fillMaxWidth()
+                    .background(MaterialTheme.colorScheme.secondary), contentAlignment = Alignment.Center
             ) {
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
+                Image(
+                    painter = painterResource(id = R.drawable.default_profile_picture),
+                    contentDescription = " Profile Picture",
                     modifier = Modifier
-                        .padding(20.dp)
-                        .fillMaxHeight(),
-                    verticalArrangement = Arrangement.SpaceBetween
+                        .size(150.dp)
+                        .clip(CircleShape)
+                )
+            }
+            Column(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
                 ) {
-                    Column (
-                        modifier = Modifier
-                            .weight(1f)
-                            .fillMaxWidth(),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.spacedBy(20.dp, Alignment.CenterVertically)
-                    ) {
-                        Box {
-                            // TODO : icon for changing profile picture (?)
-//                            Icon(
-//                                imageVector = Icons.Filled.Edit,
-//                                contentDescription = "Edit",
-//                            )
-                            Image(
-                                painter = painterResource(id = R.drawable.default_profile_picture),
-                                contentDescription = " Profile Picture",
-                                modifier = Modifier
-                                .size(150.dp)
-                                .clip(CircleShape)
-                            )
+                    Icon(
+                        Icons.Filled.Badge,
+                        contentDescription = "Name"
+                    )
+                    Text(
+                        text = "John Doe",
+                        fontSize = 18.sp
+                    )
+                }
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        Icons.Filled.Mail,
+                        contentDescription = "Name"
+                    )
+                    Text(
+                        text = "john@email.com",
+                        fontSize = 18.sp
+                    )
+                }
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Row() {
+                        Icon(
+                            Icons.Filled.DarkMode,
+                            contentDescription = "Dark Mode Toggle"
+                        )
+                        Text(
+                            text = "Dark Mode",
+                            fontSize = 18.sp
+                        )
+                    }
+                    Switch(
+                        checked = false,
+                        onCheckedChange = {
+                            // TODO: Dark mode
                         }
-                        Text(
-                            text = user.firstName + " " + user.lastName,
-                            textAlign = TextAlign.Center,
-                            fontSize = 24.sp,
-                            fontWeight = FontWeight.Bold
-                        )
-                    }
-                    Column (
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.spacedBy(15.dp)
-                    ) {
-                        HorizontalDivider(color = MaterialTheme.colorScheme.secondary)
-                        Text(
-                            text = "Kelompok 3",
-                            textAlign = TextAlign.Center,
-                        )
-                    }
+                    )
                 }
             }
-          Button(
-            onClick = {
-                onLogoutClick()
+            HorizontalDivider(color = MaterialTheme.colorScheme.secondary)
+            Column() {
+                Button(
+                    onClick = {
+                        onLogoutClick()
+                    }
+                ) {
+                    Text(text = "Logout")
+                }
+                Text(
+                    text = "Kelompok 3"
+                    // color
+                )
             }
-          ) {
-              Text(text = "Logout")
-          }
         }
     }
+//            Card(
+//                colors = CardDefaults.cardColors(
+//                    containerColor = MaterialTheme.colorScheme.surfaceVariant,
+//                ),
+//                modifier = Modifier
+//                    .size(width = 300.dp, height = 450.dp)
+//            ) {
+//                Column(
+//                    horizontalAlignment = Alignment.CenterHorizontally,
+//                    modifier = Modifier
+//                        .padding(20.dp)
+//                        .fillMaxHeight(),
+//                    verticalArrangement = Arrangement.SpaceBetween
+//                ) {
+//                    Column (
+//                        modifier = Modifier
+//                            .weight(1f)
+//                            .fillMaxWidth(),
+//                        horizontalAlignment = Alignment.CenterHorizontally,
+//                        verticalArrangement = Arrangement.spacedBy(20.dp, Alignment.CenterVertically)
+//                    ) {
+//                        Box {
+//                            Image(
+//                                painter = painterResource(id = R.drawable.default_profile_picture),
+//                                contentDescription = " Profile Picture",
+//                                modifier = Modifier
+//                                    .size(150.dp)
+//                                    .clip(CircleShape)
+//                            )
+//                        }
+//                        Text(
+////                            text = user.firstName + " " + user.lastName,
+//                            text = "John Doe", //temp hardcode for preview
+//                            textAlign = TextAlign.Center,
+//                            fontSize = 24.sp,
+//                            fontWeight = FontWeight.Bold
+//                        )
+//                    }
+//                    Column (
+//                        horizontalAlignment = Alignment.CenterHorizontally,
+//                        verticalArrangement = Arrangement.spacedBy(15.dp)
+//                    ) {
+//                        HorizontalDivider(color = MaterialTheme.colorScheme.secondary)
+//                        Text(
+//                            text = "Kelompok 3",
+//                            textAlign = TextAlign.Center,
+//                        )
+//                    }
+//                }
+//            }
 }
